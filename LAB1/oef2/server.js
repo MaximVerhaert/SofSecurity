7865cb7af567c2d47fe456dd35d2b39d113d458a
Maxim Verhaert <maxim.verhaert@student.ap.be>
2023-02-12 17:04:25 +0100

    Fixed Cors & Created Basic html website
    Fixed the cors issue and added a html page that fetches data from the server

--- LAB1/oef2/server.js
+++ LAB1/oef2/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
